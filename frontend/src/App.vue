4cf3c6ad29ba8c1874e2aa5b39efd5e2debb3fe0
Léa Cassegrain <lea.csgn@gmail.com>
2024-08-24 01:55:09 +0200

    chore: Update user authentication check in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <LoggedInView v-if="userIsAuthenticated" />
+  <LoggedInView v-if="store.user.isAuthenticated" />
   <LandingView v-else />
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
-import LoggedInView from '@/layouts/LoggedInView.vue'
-import LandingView from './layouts/LandingView.vue'
+import { onMounted } from 'vue'
 import { useUserStore } from '@/stores/user'
+import LandingView from '@/layouts/LandingView.vue'
+import LoggedInView from '@/layouts/LoggedInView.vue'
 
 const store = useUserStore()
 
-const userIsAuthenticated = computed(() => store.user.isAuthenticated)
-
 onMounted(() => {
-  store.initializeStore()
+  if (!store.user.isAuthenticated) {
+    store.initializeStore()
+  }
 })
 </script>
 
