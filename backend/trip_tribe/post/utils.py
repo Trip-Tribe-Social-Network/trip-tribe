0b7e64487bc1f6e6df4802c73321a42bfa84404e
Léa Cassegrain <lea.csgn@gmail.com>
2024-09-20 18:03:38 +0200

    chore: Fix image compression

--- backend/trip_tribe/post/utils.py
+++ backend/trip_tribe/post/utils.py
@@ -3,13 +3,13 @@ import os
 from io import BytesIO
 from django.core.files.base import ContentFile
 
-def compress_image(image, max_size_kb=100):
-    max_size_bytes = max_size_kb * 1024  # Convert KB to bytes
+def compress_image(image, max_size_kb=150):
+    max_size_bytes = max_size_kb * 1024 
     img = Image.open(image)
     
     # Resize the image to a smaller dimension before compression
     width, height = img.size
-    img.thumbnail((width // 4, height // 4))
+    img.thumbnail((width // 1.5, height // 1.5))  
 
     # Compress the image
     quality = 85  # Initial quality
@@ -24,7 +24,7 @@ def compress_image(image, max_size_kb=100):
     # If the image is still too large, resize it further
     if buffer.tell() > max_size_bytes:
         width, height = img.size
-        img.thumbnail((width // 2, height // 2))
+        img.thumbnail((width // 1.5, height // 1.5))  
         buffer = BytesIO()
         img.save(buffer, format='JPEG', optimize=True, quality=quality)
 
