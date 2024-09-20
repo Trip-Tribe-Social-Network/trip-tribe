75897e2298a961e14622223bc87c9c25188bb320
Léa Cassegrain <lea.csgn@gmail.com>
2024-09-20 13:39:20 +0200

    chore: wsgi file config for deployment

--- backend/trip_tribe/trip_tribe/wsgi.py
+++ backend/trip_tribe/trip_tribe/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add the project directory to the sys.path
+path = '/home/JennyChen10/JennyChen10.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.append(path)
 
+# Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trip_tribe.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
